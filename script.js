4ebc88b712fbce3cc87a4293f28148d35b97c8fb
Rango0504 <63256193+Rango0504@users.noreply.github.com>
2022-05-30 23:35:54 +0200

    asd

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+let flag = 0;
+const nav = document.querySelector(".navButtons");
+const bars = document.querySelector("#bars");
+bars.addEventListener("click", ()=>{
+  if(flag==0){
+    nav.classList.add("navButtonsMobile");
+    bars.classList.add("rotate");
+    flag++;
+  }
+  else{
+    nav.classList.remove("navButtonsMobile");
+    bars.classList.remove("rotate");
+    flag--;
+  }
+});
+
 const swiper = new Swiper('.swiper',{
-    speed: 1300,
+    speed: 1400,
     loop: true,
     navigation: {
       nextEl: '.swiper-button-next',
@@ -10,4 +26,5 @@ const swiper = new Swiper('.swiper',{
       disableOnInteraction: false,
       pauseOnMouseEnter: true,
     }
-  });
+  }
+);
